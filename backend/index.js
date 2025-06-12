aa4b56809d26551960b8d658d9d9efb288541621
Shreyas Patil <shreyasspatil23@gmail.com>
2025-06-12 13:24:55 +0530

    fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 
